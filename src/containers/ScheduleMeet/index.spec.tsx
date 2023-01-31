69b31c4bac821ecffa97da3fa180d585de27ee92
afroguy16 <tega_joe@hotmail.com>
2023-01-31 21:03:01 +0000

    schedule meet - add alert messages

--- src/containers/ScheduleMeet/index.spec.tsx
+++ src/containers/ScheduleMeet/index.spec.tsx
@@ -1,14 +1,94 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import ScheduleMeet from ".";
+import { SUCCESS_MESSAGE } from "./constants";
+import useBook from "./hooks/use-book";
+import { UseBookReturnPayloadI } from "./interfaces";
+
+jest.mock("./hooks/use-book");
+
 describe("ScheduleMeet", () => {
-  it("", () => {});
+  const returnedData: UseBookReturnPayloadI = {
+    bookedTimeSlots: [],
+    error: "",
+    isSuccessful: false,
+    isLoading: false,
+    onSelectDate: jest.fn(),
+    onSetError: jest.fn(),
+    onClearError: jest.fn(),
+    onClearSuccess: jest.fn(),
+    onSetBooking: jest.fn(),
+  };
+
+  afterEach(jest.resetAllMocks);
+
+  it("should display the error message if there is an error", () => {
+    const error = "fake error";
+    jest.mocked(useBook).mockReturnValue({ ...returnedData, error });
+
+    render(<ScheduleMeet />);
+
+    const errorElement = screen.getByRole("error");
+    expect(errorElement).toHaveTextContent(error);
+
+    const successElement = screen.queryByRole("success");
+    expect(successElement).not.toBeInTheDocument();
+  });
+
+  it("should display the success message if there a success message", () => {
+    jest
+      .mocked(useBook)
+      .mockReturnValue({ ...returnedData, isSuccessful: true });
+
+    render(<ScheduleMeet />);
+
+    const errorElement = screen.queryByRole("error");
+    expect(errorElement).not.toBeInTheDocument();
+
+    const successElement = screen.getByRole("success");
+    expect(successElement).toHaveTextContent(SUCCESS_MESSAGE);
+  });
+
+  it("should call onClearError if onClearMesage is triggered from the children or if the close button is clicked and there is an error message", async () => {
+    const error = "fake error";
+    const { onClearError, onClearSuccess } = returnedData;
+    jest.mocked(useBook).mockReturnValue({ ...returnedData, error });
+    const user = userEvent.setup();
+
+    render(<ScheduleMeet />);
+
+    const closeButtonElement = screen.getByRole("button", { name: /close/i });
+    await user.click(closeButtonElement);
+    expect(onClearError).toHaveBeenCalledTimes(1);
+    expect(onClearSuccess).not.toHaveBeenCalled();
+  });
+
+  it("should call onClearSuccess if onClearMesage is triggered from the children or if the close button is clicked and there is a success message", async () => {
+    const { onClearError, onClearSuccess } = returnedData;
+    jest
+      .mocked(useBook)
+      .mockReturnValue({ ...returnedData, isSuccessful: true });
+    const user = userEvent.setup();
+
+    render(<ScheduleMeet />);
+
+    const closeButtonElement = screen.getByRole("button", { name: /close/i });
+    await user.click(closeButtonElement);
+    expect(onClearSuccess).toHaveBeenCalledTimes(1);
+    expect(onClearError).not.toHaveBeenCalled();
+  });
+
+  it("should set the selected date time in the BookCall child", () => {});
+
+  it("should indicate loading if there is a network call loading", () => {});
+
+  it("should call onSelectDate when a date is selected from SelectDate", () => {});
+
+  it("should call onSetError when an error is sent from BookCall", () => {});
+
+  it("should package the payload and call onSetBooking with the payload payLoad once a Booking date is confirmed", () => {});
 });
-
-export {};
-
-// it should display error if there is an error
-// it should call onSelectDate onSelectDate from SelectDate
-// it should call onSetError onSetError from BookCall
-// it should call onClearError onClearError from BookCall and SelectDate
-// it should call onSetBooking onSendBooking from BookCall
 
 // error - should display error if there is an error
 // selectedDate - should display (child) with selected date time
@@ -16,7 +96,8 @@ export {};
 // loading - should display (child) loading when loading
 
 // onClearSuccess - should call onClear Success reset success message once timeslot or date is clicked - //TODO, replace onClearError with onClick/onFire
+// onClearError() - refer to onClearSuccess TODO
+
 // onSelectDate(date) - should call onSelectDate onSelectDate from SelectDate
 // onSetError(error) - should call onSetError onSetError from BookCall
-// onClearError() - refer to onClearSuccess TODO
 // onSetBooking(booking) - should call onSetBooking with payLoad that includes selectedDate {date: day, time: time, reasonforcall: string}
--- src/containers/ScheduleMeet/index.tsx
+++ src/containers/ScheduleMeet/index.tsx
@@ -1,7 +1,16 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Box,
+  CloseButton,
+  Flex,
+} from "@chakra-ui/react";
 import useBook from "./hooks/use-book";
 import BookCall from "./components/BookCall";
 import SelectDate from "./components/SelectDate";
+import { SUCCESS_MESSAGE } from "./constants";
 
 const ScheduleMeet = () => {
   const {
@@ -12,18 +21,59 @@ const ScheduleMeet = () => {
     onSelectDate,
     onSetError,
     onClearError,
+    onClearSuccess,
     onSetBooking,
   } = useBook();
 
+  const hasMessage = !!error || isSuccessful;
+
+  const onClearMessage = () => {
+    if (error) {
+      return onClearError();
+    }
+    onClearSuccess();
+  };
+
   return (
-    <Box>
-      <SelectDate onSelectDate={() => {}} onClearMessages={() => {}} />
-      <BookCall
-        unavailableTimeSlots={bookedTimeSlots}
-        onBookCall={() => {}}
-        onSendError={() => {}}
-        onClearMessages={() => {}}
-      />
+    <Box paddingTop={"20px"}>
+      {hasMessage && (
+        <Flex justifyContent={"center"} marginBottom={"20px"}>
+          <Alert
+            status={!!error ? "error" : "success"}
+            borderRadius={"4px"}
+            width="50%"
+            flexDirection="row"
+            justifyContent="space-between"
+            role={!!error ? "error" : "success"}
+          >
+            <Flex flexDirection="row">
+              <AlertIcon />
+              <Box>
+                <AlertTitle>{!!error ? "Error" : "Success"}</AlertTitle>
+                <AlertDescription>
+                  {!!error ? error : SUCCESS_MESSAGE}
+                </AlertDescription>
+              </Box>
+            </Flex>
+            <CloseButton
+              alignSelf="flex-start"
+              position="relative"
+              right={-1}
+              top={-1}
+              onClick={onClearMessage}
+            />
+          </Alert>
+        </Flex>
+      )}
+      <Box>
+        <SelectDate onSelectDate={() => {}} onClearMessages={() => {}} />
+        <BookCall
+          unavailableTimeSlots={bookedTimeSlots}
+          onBookCall={() => {}}
+          onSendError={() => {}}
+          onClearMessages={() => {}}
+        />
+      </Box>
     </Box>
   );
 };
